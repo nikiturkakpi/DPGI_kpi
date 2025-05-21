c6f932ab0fe33e8f4fad8b2a133adbbc0a6859ac
Nikita Turda <turda.nikita@lll.kpi.ua>
2025-05-21 22:13:24 +0300

    developed WPF-app "Digital Alarm"

---
+++ Labka3/Labka3/AlarmWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+
+namespace Labka3
+{
+    public partial class AlarmWindow : Window
+    {
+        public DateTime SelectedTime { get; private set; }
+
+        public AlarmWindow()
+        {
+            InitializeComponent();
+            InitializeTimeSelectors();
+        }
+
+        private void InitializeTimeSelectors()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                HoursComboBox.Items.Add(i.ToString("00"));
+            }
+            HoursComboBox.SelectedIndex = DateTime.Now.Hour;
+
+            for (int i = 0; i < 60; i++)
+            {
+                MinutesComboBox.Items.Add(i.ToString("00"));
+                SecondsComboBox.Items.Add(i.ToString("00"));
+            }
+            MinutesComboBox.SelectedIndex = DateTime.Now.Minute;
+            SecondsComboBox.SelectedIndex = 0;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            var time = new DateTime(
+                DateTime.Now.Year,
+                DateTime.Now.Month,
+                DateTime.Now.Day,
+                int.Parse(HoursComboBox.SelectedItem.ToString()),
+                int.Parse(MinutesComboBox.SelectedItem.ToString()),
+                int.Parse(SecondsComboBox.SelectedItem.ToString()));
+
+            SelectedTime = time;
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
---
+++ Labka3/Labka3/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Labka3
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _clockTimer;
+        private DateTime _alarmTime;
+        private bool _alarmSet;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeClock();
+        }
+
+        private void InitializeClock()
+        {
+            _clockTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _clockTimer.Tick += UpdateClock;
+            _clockTimer.Start();
+        }
+
+        private void UpdateClock(object sender, EventArgs e)
+        {
+            var currentTime = DateTime.Now;
+
+            HourLabel.Text = currentTime.ToString("HH");
+            MinuteLabel.Text = currentTime.ToString("mm");
+            SecondLabel.Text = currentTime.ToString("ss");
+            DayLabel.Text = currentTime.ToString("ddd").ToUpper();
+            DateLabel.Text = currentTime.ToString("dd/MM/yyyy");
+
+            if (_alarmSet && currentTime.Hour == _alarmTime.Hour &&
+                currentTime.Minute == _alarmTime.Minute &&
+                currentTime.Second == _alarmTime.Second)
+            {
+                TriggerAlarm();
+            }
+        }
+
+        private void SetAlarm_Click(object sender, RoutedEventArgs e)
+        {
+            var alarmWindow = new AlarmWindow();
+            if (alarmWindow.ShowDialog() == true)
+            {
+                _alarmTime = alarmWindow.SelectedTime;
+                _alarmSet = true;
+                MessageBox.Show($"Alarm set for {_alarmTime:HH:mm:ss}\nDescription: {AlarmDescription.Text}");
+            }
+        }
+
+        private void TriggerAlarm()
+        {
+            _alarmSet = false;
+            MessageBox.Show($"ALARM!\n{AlarmDescription.Text}", "Alarm", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
